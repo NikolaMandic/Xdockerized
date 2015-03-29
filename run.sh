5e1d77a89bae702a9733f7b0efbde730663a0d2b
Никола <mn080202@gmail.com>
2015-03-29 21:52:19 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo docker run -it --rm -v `pwd`/start.sh:/root/start.sh -v `pwd`/v.sh:/root/.vnc/xstartup -v `pwd`/save:/root/save --cpuset="0,1,2,3" --name  "ds" -p 5901:5901 vn:xfce3
+sudo docker run -it --rm -v `pwd`/start.sh:/root/start.sh -v `pwd`/v.sh:/root/.vnc/xstartup -v `pwd`/save:/root/save --cpuset="0,1,2,3" --name  "ds" -p 5901:5901 vn
